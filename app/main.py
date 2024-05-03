edf30d42e351acf164367b06ab46e245aafd5258
yoona122 <162417329+yoona122@users.noreply.github.com>
2024-05-03 15:38:09 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,21 +1,78 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, Column, Interger, String
+from squlalchmy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel
 
 import requests
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///../todo.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 app = FastAPI()
 
 
+class Todo(Base):
+    __tablename__ = "todos"
+    __allow_unmapped__ = True
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+
+Base.metadata.create_all(bind=engine)
+
+class TodoCreate(BaseModel):
+    title: str
+    description: str
+
+class TodoUpdate(BaseModel):
+    title: str
+    description: str
+    
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/")
-def root():
-    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&bizCd=C&apiKey=mtJCVe56P691oLNQZWcw6qpja525iDOR66z2iAMN&returnType=json"
+@app.post("/todos/")
+def create_todo(todo: TodoCreate, db: Session = Depends(get_db)):
+    db_todo = Todo(title=todo.title, description=todo.description)
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
 
-    contents = requests.get(URL).text
+@app.get("/todos/{todo_id}")
+def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
 
-    return {"message":contents}
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: int, todo: TodoUpdate, db: Session = Depends(get_db)):
+    db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    db_todo.title = todo.title
+    db_todo.description = todo.description
+    db.commit()
+    return db_todo
 
-
-@app.get("/home")
-def home():
-    return {"message":"Home!"}
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    db.delete(db_todo)
+    db.commit()
+    return {"ok": True}
